b6a8140d66e8a4bdbcd2d094d78c5021c06a93ff
Simon Senic <simon.senic@kosickaakademia.sk>
2020-10-15 20:30:00 +0200

    Compute result

--- MainClass.java
+++ MainClass.java
@@ -6,5 +6,22 @@ public class MainClass {
         System.out.println(num.isBinaryNumber(1));
         System.out.println(num.isOctalNumber(1236478));
         System.out.println(num.isHexadecimalNumber("1F010ABC9"));
+
+        System.out.println();
+
+        EvenNumber en = new EvenNumber();
+        int[] array = new int[] { 1, 2, 3, 4, 8, 11, 15, 12, 13, 17, 23, 19 };
+        int[] array2 = en.evenNumberArray(array);
+        int length2 = array2.length;
+        for(int a=0; a<length2; a++)
+            System.out.print(array2[a] +" ");
+
+        System.out.println();
+
+        RandomNumber rn = new RandomNumber();
+        rn.guessNumber(3); System.out.println();
+        rn.computeResult(10);
+
+
     }
 }
